6c76aeb9aa2cfc2b7fdd27d1867acfada3766c84
Jannat Rashid <97726066+jannatRashid@users.noreply.github.com>
2023-02-27 16:46:26 +0500

    Add files via upload

---
+++ efficency.py
@@ -0,0 +1,12 @@
+print("program to tell the efficiency of worker")
+v=int(input("enter working time of the worker   "))
+if (v>=2 and v<=3):
+    print("worker is highly efficient")
+elif (v>=3 and v<=4):
+    print("should improve speed")
+elif (v>=4 and v<=5):
+    print("the worker is given traning to improve speed")
+
+else:
+    print("the worker has to leave the company")
+    
